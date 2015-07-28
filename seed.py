03c576972fd9132edd64b4c974ee50f44d868186
Hackbright Student <info@hackbrightacademy.com>
2015-07-28 11:38:07 -0700

    we have a db of movies, users and ratings.

--- seed.py
+++ seed.py
@@ -2,8 +2,7 @@
 
 from model import User, Rating, Movie, connect_to_db, db
 from server import app
-import datetime
-import calendar
+from datetime import datetime
 
 def load_users():
     """Load users from u.user into database."""
@@ -19,7 +18,8 @@ def load_users():
         zipcode_value = split_data[4]
         temp_user_object = User(user_id = user_id_value, age = age_value, zipcode = zipcode_value)
         db.session.add(temp_user_object)
-        db.session.commit()
+    
+    db.session.commit()
 
     print "You did it!"
    
@@ -37,39 +37,44 @@ def load_movies():
         movie_id_value = split_data[0]
         title_value = split_data[1]
         title_value = title_value[:-7]
-        print title_value
         release_date_value = split_data[2]
-        release_date_value = (release_date_value).strip("-")
 
-        date_text = "13SEP2014"
-        date = datetime.datetime.strptime(date_text, "%d%b%Y")
-
-        print(date)
-        print(calendar.timegm(date.utctimetuple()))
-
-
-
+        #Checks for movies with no release date, which otherwise throw an error
+        if release_date_value == "":
+            continue
+        else:
+            release_date_value = datetime.strptime(release_date_value, "%d-%b-%Y")
+        
         imdb_url_value = split_data[4]
+
         temp_item_object = Movie(movie_id = movie_id_value, title = title_value, release_date = release_date_value, imdb_url = imdb_url_value)
         db.session.add(temp_item_object)
-        db.session.commit()     
+    
+    db.session.commit()     
 
     print "Movies happened!"   
-
-#current mystery: strptime is not in datetime? How do we call it? Importing datetime is not enough. 
-#Format for our date: "%d %b %Y"
-#maybe start here: https://docs.python.org/2/library/datetime.html
-
-# movie id | movie title | release date | video release date | IMDb URL |
-# Item
-# movie_id: INT, shared with Data table, unique primary key
-# title: VARCHAR(60), NOT NULL
-# release date: INT, NOT NULL
-# IMDB URL: VARCHAR(150)
 
 def load_ratings():
     """Load ratings from u.data into database."""
 
+    file_open = open("seed_data/u.data")
+
+    for line in file_open:
+        temp_data = line.rstrip()
+        split_data = temp_data.split("\t")
+        user_id_value = split_data[0]
+        movie_id_value = split_data[1]
+        score_value = split_data[2]
+
+        temp_rating_object = Rating(user_id = user_id_value, movie_id = movie_id_value, score = score_value)
+        db.session.add(temp_rating_object)
+
+    db.session.commit()
+
+    print "We have ratings, yeah!"
+
+#user_id \t movie_id \t score \t timestamp
+#We only want the first three
 
 if __name__ == "__main__":
     connect_to_db(app)
